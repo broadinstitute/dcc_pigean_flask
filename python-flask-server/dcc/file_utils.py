7692a3c4a36598581e3bed9ea68b81837d2d8a33
Marc Duby <mduby@broadinstitute.org>
2024-06-17 21:38:29 -0400

    test: added file reading

---
+++ python-flask-server/dcc/file_utils.py
@@ -0,0 +1,53 @@
+
+# imports
+import json
+
+# methods
+def read_tab_delimited_file(file_path, log=False):
+    '''
+    will read the pathway file and build a map of gene lists
+    '''
+    # initialize
+    result = {}
+
+    # read in the data
+    with open(file_path, 'r') as file:
+        for line in file:
+            fields = line.strip().split('\t')
+            key = fields[0]
+            values = fields[1:]
+            if key in result:
+                result[key].extend(values)
+            else:
+                result[key] = values
+
+    # return
+    return result
+
+
+def load_gene_file_into_map(file_path, log=False):
+    '''
+    will read the gene file and return a map of gene to position in array
+    '''
+    # initialize
+    result = {}
+    count = 0
+
+    # read in the data
+    with open(file_path, 'r') as file:
+        for line in file:
+            gene = line.strip()
+            result[gene] = count
+            count = count + 1
+
+    # return
+    return result
+
+
+
+
+if __name__ == "__main__":
+
+    file_path = '/home/javaprog/Code/TranslatorWorkspace/PigeanFlask/python-flask-server/conf/gene_lists/gene_set_list_msigdb_h.txt'
+    data = read_tab_delimited_file(file_path)
+    print(json.dumps(data, indent=2))
---
+++ python-flask-server/test/dcc/test_file_utils.py
@@ -0,0 +1,68 @@
+
+# imports
+import pytest 
+import json 
+
+import dcc.file_utils as futils
+
+# constants
+gene_set_file = "python-flask-server/test/dcc/gene_set_list_test.txt"
+gene_file = "python-flask-server/test/dcc/gene_test.txt"
+
+# test methods
+def test_read_tab_delimited_file():
+    '''
+    test file reading into a map
+    '''
+    # initialize
+    map_result = {}
+
+    # get the data
+    map_result = futils.read_tab_delimited_file(file_path=gene_set_file)
+
+    # test
+    assert len(map_result) == 8
+    assert map_result.get('HALLMARK_UNFOLDED_PROTEIN_RESPONSE') is not None
+    assert len(map_result.get('HALLMARK_UNFOLDED_PROTEIN_RESPONSE')) == 17
+
+
+def test_load_gene_file_into_map():
+    '''
+    test file reading into a map
+    '''
+    # initialize
+    map_result = {}
+
+    # get the data
+    map_result = futils.load_gene_file_into_map(file_path=gene_file)
+
+    # test
+    assert len(map_result) == 200
+    assert map_result.get('CXCL10') is not None
+    assert map_result.get('CXCL10') == 56
+
+
+# def test_get_querygraph_key_node():
+#     '''
+#     test the qnode retrieval
+#     '''
+#     # initialize
+#     query: Query = Query.from_dict(json_data)
+#     key = None 
+#     qnode: QNode = None
+
+#     # get the data
+#     key, qnode = textract.get_querygraph_key_node(trapi_query=query)
+
+#     # test
+#     assert key == "subj"
+#     assert qnode.ids == ["MONDO:0011936"]
+#     assert qnode.set_interpretation == "BATCH"
+
+#     # get the data
+#     key, qnode = textract.get_querygraph_key_node(trapi_query=query, is_subject=False)
+
+#     # test
+#     assert key == "obj"
+#     assert qnode.ids is None
+#     assert qnode.set_interpretation is None
