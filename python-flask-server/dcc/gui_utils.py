6181157b5e1dcd5c4d89725e1b19511079d2b48f
Marc Duby <mduby@broadinstitute.org>
2024-09-24 14:23:03 -0400

    pigean: initial commit

--- python-flask-server/dcc/gui_utils.py
+++ python-flask-server/dcc/gui_utils.py
@@ -96,6 +96,7 @@ def gui_build_results_map(list_factor, list_factor_genes, list_factor_gene_sets,
     # return
     return map_result
 
+
 def gui_build_novelty_results_map(map_gene_ontology, list_input_gene_names, map_gene_index, matrix_gene_sets, map_gene_novelty, log=False):
     '''
     builds the map results
@@ -133,6 +134,76 @@ def gui_build_novelty_results_map(map_gene_ontology, list_input_gene_names, map_
 
     # add the input genes to the map
     map_result['gene_results'] = map_result_genes
+
+    # return
+    return map_result
+
+def build_gui_pigean_app_results_map(list_factor, list_factor_genes, list_factor_gene_sets, map_gene_ontology, list_input_gene_names, map_gene_index, matrix_gene_sets, map_gene_novelty, log=False):
+    '''
+    root method to build the pigean app results 
+    '''
+    # initialize
+    map_result = {}
+
+    # build the subsets of the data
+    pigean_factor_map = build_pigean_factor_results_map(list_factor=list_factor, list_factor_genes=list_factor_genes, list_factor_gene_sets=list_factor_gene_sets, log=log)
+    map_result['pigean-factor'] = pigean_factor_map
+
+    # return
+    return map_result
+    
+
+def build_pigean_factor_results_map(list_factor, list_factor_genes, list_factor_gene_sets, map_gene_ontology, list_input_gene_names, map_gene_index, matrix_gene_sets, map_gene_novelty, log=False):
+    '''
+    builds the map results for the pigean app factor subset
+    '''
+    # initialize
+    map_result = {}
+
+    # add the data
+    list_result = []
+
+    # loop through the factors
+    for index, row in enumerate(list_factor):
+        list_result.append({'top_set': row, 'gene_sets': list_factor_gene_sets[index], 'genes': list_factor_genes[index]})
+
+    # add the factors to the map
+    map_result['data'] = list_result
+
+    # log
+    logger.info("added factor data to REST of size: {}".format(len(list_result)))
+
+    # add the input genes
+    map_result_genes = {}
+    for gene_name in list_input_gene_names:
+        # get the ontology_id
+        ontology_id = map_gene_ontology.get(gene_name)
+        if ontology_id:
+            map_result_genes[ontology_id] = {'name': gene_name, 'ontology_id': ontology_id}
+
+            # annotate the gene data
+            index_gene = map_gene_index.get(gene_name)
+            if index_gene:
+                # get the number of pathways the gene is in
+                count_pathways = mutils.sum_of_gene_row(sparse_matrix=matrix_gene_sets, gene_index=index_gene).item()
+                map_result_genes.get(ontology_id)['count_pathways'] = count_pathways
+
+                # add the novelty score to the gene
+                novelty_score = map_gene_novelty.get(gene_name)
+                if not novelty_score:
+                    novelty_score = 0.0
+                map_result_genes.get(ontology_id)['novelty_score'] = novelty_score
+
+                if log:
+                    logger.info("for gene: {}, got index: {}, pathway count: {}, novelty score: {}".format(gene_name, index_gene, count_pathways, novelty_score))
+
+
+    logger.info("got novelty gene map of size: {}".format(len(map_gene_novelty)))
+    logger.info("returning result gene map of size: {}".format(len(map_result_genes)))
+
+    # add the input genes to the map
+    map_result['input_genes'] = list_input_gene_names
+    map_result['input_genes_map'] = map_result_genes
 
     # return
     return map_result
